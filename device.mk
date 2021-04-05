9ae7ac42fccba8c2d1fd9fcb8222bb75272e0bbf
Nicholas Lim <nicknitewolf@lineageos.org>
2021-04-06 01:29:06 +0800

    g151: Get rid of Lineage specific overlays folder
     * Partially revert:
         wt88047: Move Lineage specific overlays to overlay-lineage
    
         Change-Id: Ic38bb1acb4d37f90b62679e4420815f49b30fc7f
    
    Change-Id: Ifc4f83422c8dda7581f12c1ef802629452d6310e

--- device.mk
+++ device.mk
@@ -18,9 +18,7 @@
 $(call inherit-product, vendor/haier/g151/g151-vendor.mk)
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
